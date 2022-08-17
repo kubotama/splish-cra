57de9578985d1ffd3716b55ec10d96d231cddb40
kubotama <16697573+kubotama@users.noreply.github.com>
2022-08-17 21:38:11 +0900

    音声を合成したテキストと合成した音声を保存したファイル名の表示をテストした。
    メインプロセスのメソッドはモックとした。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
+
+import { SplishIpc } from "./SplishIpc";
+
 import "./App.css";
 
 function App() {
+  const [synthesizedText, setSynthesizedText] = useState("");
+  const [speechFilename, setSpeechFilename] = useState("");
+
+  useEffect(() => {
+    SplishIpc.getSynthesizedInfo().then((synthesizedInfo) => {
+      setSynthesizedText(synthesizedInfo.text);
+      setSpeechFilename(synthesizedInfo.filename);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">SPLISH</header>
-      <output data-testid="synthesizedText"></output>
-      <div hidden data-testid="synthesizedFilename"></div>
+      <textarea
+        data-testid="synthesizedText"
+        defaultValue={synthesizedText}
+      ></textarea>
+      <div hidden data-testid="synthesizedFilename">
+        {speechFilename}
+      </div>
     </div>
   );
 }
---
+++ src/GetSynthesizedInfo.test.tsx
@@ -0,0 +1,90 @@
+import { render, screen, act, waitFor } from "@testing-library/react";
+import App from "./App";
+
+import "@testing-library/jest-dom";
+
+import { SplishIpc } from "./SplishIpc";
+
+jest.mock("./SplishIpc.ts");
+const mockSplishIpc = SplishIpc as jest.Mocked<typeof SplishIpc>;
+
+describe("起動時に、直前に音声に合成したテキストと、音声を保存したファイル名を取得する。", () => {
+  beforeEach(() => {
+    mockSplishIpc.getSynthesizedInfo.mockClear();
+  });
+
+  test("テキストとファイル名の領域が存在する。", () => {
+    // Arrange
+    mockSplishIpc.getSynthesizedInfo.mockResolvedValue({
+      text: "",
+      filename: "",
+    });
+
+    // Act
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      render(<App />);
+    });
+
+    // Assert
+    // テキストの領域が存在する
+    const synthesizedText = screen.getByTestId("synthesizedText");
+    expect(synthesizedText).toBeInTheDocument();
+
+    // ファイル名の領域が存在する
+    const synthesizedFilename = screen.getByTestId("synthesizedFilename");
+    expect(synthesizedFilename).toBeInTheDocument();
+  });
+
+  test("splish.jsonがない場合には、テキストとファイル名とも''にを設定する。", () => {
+    // Arrange
+    mockSplishIpc.getSynthesizedInfo.mockResolvedValue({
+      text: "",
+      filename: "",
+    });
+
+    // Act
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      render(<App />);
+    });
+    // Assert
+
+    // テキストに""が設定されている
+    const synthesizedText = screen.getByTestId("synthesizedText");
+    expect(synthesizedText.textContent).toBe("");
+
+    // ファイル名に""が設定されている
+    const synthesizedFilename = screen.getByTestId("synthesizedFilename");
+    expect(synthesizedFilename.textContent).toBe("");
+  });
+
+  test("splish.jsonがある場合には、splish.jsonのテキストとファイル名を設定する。", async () => {
+    // Arrange
+    mockSplishIpc.getSynthesizedInfo.mockResolvedValue({
+      text: "Before we can enable the visualizations, we need to install the Lighthouse plugin. Skip this step if you're already using it! Otherwise, navigate to Plugins and search for Lighthouse. Click Install.",
+      filename: "speech.mp3",
+    });
+
+    // Act
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    act(() => {
+      render(<App />);
+    });
+
+    // Arrange
+    const synthesizedText = screen.getByTestId("synthesizedText");
+    const synthesizedFilename = screen.getByTestId("synthesizedFilename");
+
+    // テキストが設定されている
+    await waitFor(() => {
+      expect(synthesizedText).toHaveValue(
+        "Before we can enable the visualizations, we need to install the Lighthouse plugin. Skip this step if you're already using it! Otherwise, navigate to Plugins and search for Lighthouse. Click Install."
+      );
+    });
+    // ファイル名が設定されている
+    await waitFor(() => {
+      expect(synthesizedFilename.textContent).toBe("speech.mp3");
+    });
+  });
+});
