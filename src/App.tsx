65140ccbd5207c255d298197ee1a82c5d2c37670
kubotama <16697573+kubotama@users.noreply.github.com>
2022-08-21 14:19:02 +0900

    テキストの入力エリアに文字列が入力されているときに合成ボタンを有効にした。

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import "./App.css";
 function App() {
   const [synthesizedText, setSynthesizedText] = useState("");
   const [speechFilename, setSpeechFilename] = useState("");
+  const [inputText, setInputText] = useState("");
+  const [synthesizeButtonDisabled, setSynthesizeButtonDisabled] =
+    useState(true);
 
   useEffect(() => {
     SplishIpc.getSynthesizedInfo().then((synthesizedInfo) => {
@@ -15,14 +18,28 @@ function App() {
     });
   }, []);
 
+  const onChangeInputText = (e: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setInputText(e.target.value);
+    const text = e.target.value.toString();
+    setSynthesizeButtonDisabled(text.length === 0 ? true : false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">SPLISH</header>
-      <textarea className="inputText" data-testid="inputText" />
+      <textarea
+        className="inputText"
+        placeholder="合成するテキストを入力して下さい"
+        data-testid="inputText"
+        value={inputText}
+        onChange={onChangeInputText}
+      />
       <button
         className="synthesizeButton"
         data-testid="synthesizeButton"
-        disabled
+        disabled={synthesizeButtonDisabled}
       >
         合成
       </button>
--- src/tests/TextToSynthesize.test.tsx
+++ src/tests/TextToSynthesize.test.tsx
@@ -1,7 +1,8 @@
-import { render, screen, act, waitFor } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import App from "../App";
 
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 
 import { SplishIpc } from "../SplishIpc";
 
@@ -53,8 +54,28 @@ describe("入力したテキストから音声に合成する。", () => {
     expect(synthesizeButton).toBeDisabled();
   });
 
-  test("テキストの入力エリアに文字列が入力されているときは合成ボタンが有効である。", () => {
-    // TODO: テキストの入力エリアに文字列が入力されているときは合成ボタンが有効である。
+  test("テキストの入力エリアに文字列が入力されているときは合成ボタンが有効である。", async () => {
+    // Arrange
+    mockSplishIpc.getSynthesizedInfo.mockResolvedValue({
+      text: "",
+      filename: "",
+    });
+    render(<App />);
+    // const inputText = screen.getByTestId("inputText");
+    const inputText =
+      screen.getByPlaceholderText("合成するテキストを入力して下さい");
+    const text =
+      "Today's Changelog brings improved date filtering and the command palette (beta) to Projects!";
+
+    // Act
+    // テキストの入力エリアに文字列を入力する
+    await userEvent.type(inputText, text);
+
+    // Assert
+    // テキストの入力エリアに文字列が入力されているときは合成ボタンが有効である。
+    expect(inputText).toHaveValue(text);
+    const synthesizeButton = screen.getByTestId("synthesizeButton");
+    expect(synthesizeButton).toBeEnabled();
   });
 
   test("テキストの入力エリアに文字列が入力されているときに合成ボタンをクリックする", () => {
