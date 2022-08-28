e2da94fd84a0706db779ce12dbc9312432adaa22
kubotama <16697573+kubotama@users.noreply.github.com>
2022-08-28 14:21:42 +0900

    feat: 全角のアポストロフィを半角に変換する。
    テストケースを追加した。
    - アポストロフィが一つの場合
    - アポストロフィが二つの場合

--- src/tests/TextToSynthesize.test.tsx
+++ src/tests/TextToSynthesize.test.tsx
@@ -78,40 +78,62 @@ describe("入力したテキストから音声に合成する。", () => {
     expect(synthesizeButton).toBeEnabled();
   });
 
-  test("テキストの入力エリアに文字列が入力されているときに合成ボタンをクリックする", async () => {
-    // Arrange
-    mockSplishIpc.loadConfiguration.mockResolvedValue({
-      text: "",
-      filename: "",
-    });
-    mockSplishIpc.textToSynthesize.mockResolvedValue("speech.mp3");
-    render(<App />);
-    // const inputText = screen.getByTestId("inputText");
-    const inputText =
-      screen.getByPlaceholderText("合成するテキストを入力して下さい");
-    const synthesizeButton = screen.getByTestId("synthesizeButton");
-    const synthesizedText = screen.getByTestId("synthesizedText");
-    const synthesizedFilename = screen.getByTestId("synthesizedFilename");
-    const text =
-      "Today's Changelog brings improved date filtering and the command palette (beta) to Projects!";
+  test.each([
+    {
+      input:
+        "Today's Changelog brings improved date filtering and the command palette (beta) to Projects!",
+      expected:
+        "Today's Changelog brings improved date filtering and the command palette (beta) to Projects!",
+    },
+    {
+      input:
+        "And if you’re new to Lighthouse Scores, or want to learn more about how they're calculated, check out the Developer's Intro to Core Web Vitals.",
+      expected:
+        "And if you're new to Lighthouse Scores, or want to learn more about how they're calculated, check out the Developer's Intro to Core Web Vitals.",
+    },
+    {
+      input:
+        "And if you’re new to Lighthouse Scores, And if you’re new to Lighthouse Scores,",
+      expected:
+        "And if you're new to Lighthouse Scores, And if you're new to Lighthouse Scores,",
+    },
+  ])(
+    "テキストの入力エリアに文字列が入力されているときに合成ボタンをクリックする: $input",
+    async ({ input, expected }) => {
+      // Arrange
+      mockSplishIpc.loadConfiguration.mockResolvedValue({
+        text: "",
+        filename: "",
+      });
+      mockSplishIpc.textToSynthesize.mockResolvedValue("speech.mp3");
+      render(<App />);
+      // const inputText = screen.getByTestId("inputText");
+      const inputText =
+        screen.getByPlaceholderText("合成するテキストを入力して下さい");
+      const synthesizeButton = screen.getByTestId("synthesizeButton");
+      const synthesizedText = screen.getByTestId("synthesizedText");
+      const synthesizedFilename = screen.getByTestId("synthesizedFilename");
+      // const text =
+      //   "Today's Changelog brings improved date filtering and the command palette (beta) to Projects!";
 
-    // Act
-    // テキストの入力エリアに文字列を入力する
-    await userEvent.type(inputText, text);
-    // 合成ボタンをクリックする
-    await userEvent.click(synthesizeButton);
+      // Act
+      // テキストの入力エリアに文字列を入力する
+      await userEvent.type(inputText, input);
+      // 合成ボタンをクリックする
+      await userEvent.click(synthesizeButton);
 
-    // Assert
-    // 入力エリアの文字列が合成されたテキストのエリアに表示される。
-    expect(synthesizedText).toHaveValue(text);
-    // 入力エリアの文字列がクリアされる。
-    expect(inputText).toHaveValue("");
-    // 合成ボタンが無効になる。
-    expect(synthesizeButton).toBeDisabled();
-    // 合成された音声を保存したファイル名が設定されている。
-    expect(synthesizedFilename.textContent).toBe("speech.mp3");
+      // Assert
+      // 入力エリアの文字列が合成されたテキストのエリアに表示される。
+      expect(synthesizedText).toHaveValue(expected);
+      // 入力エリアの文字列がクリアされる。
+      expect(inputText).toHaveValue("");
+      // 合成ボタンが無効になる。
+      expect(synthesizeButton).toBeDisabled();
+      // 合成された音声を保存したファイル名が設定されている。
+      expect(synthesizedFilename.textContent).toBe("speech.mp3");
 
-    expect(mockSplishIpc.textToSynthesize).toBeCalledTimes(1);
-    expect(mockSplishIpc.textToSynthesize).toBeCalledWith(text);
-  });
+      expect(mockSplishIpc.textToSynthesize).toBeCalledTimes(1);
+      expect(mockSplishIpc.textToSynthesize).toBeCalledWith(expected);
+    }
+  );
 });
